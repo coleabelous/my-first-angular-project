1abda4344bd451f2d2bc14a5ed8de33e3845bde8
Colea Belous <colea.belous@outlook.com>
2020-06-17 16:08:16 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,26 @@
+import { Component,  } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent {
+
+  carName = '';
+  addCarStatus = false;
+  cars = ['Ford', 'Audi', 'BMV', 'Mazda', 'Lada', 'Bently'];
+
+
+  constructor() {
+
+  }
+
+  addCar() {
+    this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
+  }
+
+
+}
